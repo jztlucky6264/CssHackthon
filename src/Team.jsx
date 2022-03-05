2ee05f81669b0bf4181f7b50cbbba6b5c0219704
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2022-03-05 12:54:20 +0530

    second commit

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,16 +1,45 @@
 import React from "react";
-import web from "../src/Images/About.jpg";
-import CommonPage1 from "./CommonPage1";
 
-const Team = () => {
+const Team = (props) => {
   return (
     <>
-      <CommonPage1
-        name=" ADD YOUR TEAM WITH"
-        imgsrc={web}
-        visit="/contact"
-        btname="Contact Now"
-      />
+      <div className="team_main m-5">
+        <div className="col-md-4 mb-5 col-12 mx-auto">
+          <div className="card ">
+            <div className="card-img">
+              <img
+                className="card-img-top overflow-hidden"
+                src="https://img.freepik.com/free-vector/man-profile-cartoon_18591-58482.jpg?size=338&ext=jpg"
+                alt="Card cap"
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title font-weight-bold">
+                We are the team of senior developers
+              </h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4 col-10 mx-auto">
+          <div className="card  ">
+            <div className="card-img">
+              <img
+                className="card-img-top overflow-hidden"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShfAkt-YyhLLslLZuhPTLihE8o0sQuccbb1A&usqp=CAU"
+                alt="Card cap"
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title font-weight-bold">
+                We are the team of senior developers
+              </h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
